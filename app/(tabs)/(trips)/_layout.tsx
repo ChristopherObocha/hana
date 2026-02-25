1420973f864fe275794941f63007980f4a5c0bf7
Christopher Obocha <chris@runwae.io>
2026-02-25 02:13:08 +0000

    Trip Details UI WIP

--- app/(tabs)/(trips)/_layout.tsx
+++ app/(tabs)/(trips)/_layout.tsx
@@ -4,6 +4,7 @@ export default function TripsLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="[tripId]" options={{ headerShown: false }} />
     </Stack>
   );
 }
